cc13636cd0c8debfa3e09cbd70e107c320725862
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-11-21 03:18:05 +0000

    crear signup en la raiz

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import backgroundImage from '../../img/background.jpg';
 
 
---
+++ src/front/js/component/signup.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Signup = () => {
+
+
+
+    return (
+        <>
+        <div>
+            <div className="container mt-5">
+                
+            </div>
+        </div>
+        </>
+    );
+};
