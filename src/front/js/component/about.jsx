188cec9092c0f6a27d7021ee6d0f62e81bd75a82
gonchip <136641475+gonchip@users.noreply.github.com>
2023-11-29 04:41:40 +0000

    faltan detalles about

--- src/front/js/component/about.jsx
+++ src/front/js/component/about.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import aboutImage from '../../img/about.jpg';
+import carrImage1 from '../../img/carr.jpg';
+import carrImage2 from '../../img/carr2.jpg';
+import carrImage3 from '../../img/carr3.jpg';
 
 const backgroundStyle = {
   backgroundImage: `url(${aboutImage})`,
@@ -13,6 +16,15 @@ const backgroundStyle = {
   fontFamily: 'Unbounded, sans-serif',
 };
 
+const carouselContainerStyle = {
+  position: 'fixed',
+  bottom: '10px',
+  left: '10px',
+  maxWidth: '80vh',
+  marginLeft:'17vh',
+  marginBottom:'4vh',
+};
+
 export const About = () => {
   return (
     <div style={backgroundStyle} className="text-white">
@@ -23,12 +35,51 @@ export const About = () => {
         estamos aquí para inspirarte y facilitar tus aventuras. <br></br>
         Descubre el mundo con RGGN!</p>
 
-        <h1 className="d-flex justify-content-end me-5">QUIENES SOMOS?</h1>
-        <p className="d-flex justify-content-end me-3"> Somos un equipo pequeño con un gran propósito.<br></br> 
+        <h1 className="d-flex justify-content-end me-5 pe-5">QUIENES SOMOS?</h1>
+        <p className="d-flex justify-content-end me-3 pe-5"> Somos un equipo pequeño con un gran propósito.<br></br> 
         Te ayudamos a descubrir tu destino perfecto. <br></br>
         Simplificamos el proceso para que tu <br></br>
         experiencia sea única y sin complicaciones. </p>
-    </div>
+
+        <div style={carouselContainerStyle}>
+        <div id="carouselExampleCaptions" className="carousel slide carousel-fade">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={carrImage1} className="d-block w-100 rounded-5" alt="Primera imagen" />
+              <div className="carousel-caption d-none d-md-block">
+                <p>Descubre con RGGN</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={carrImage2} className="d-block w-100 rounded-5" alt="Segunda imagen" />
+              <div className="carousel-caption d-none d-md-block">
+                <p>Tu Viaje, Tu Estilo</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={carrImage3} className="d-block w-100 rounded-5" alt="Tercera imagen" />
+              <div className="carousel-caption d-none d-md-block">
+                <p>Exploración Sin Límites</p>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+        </div>
+    
   );
 };
 
